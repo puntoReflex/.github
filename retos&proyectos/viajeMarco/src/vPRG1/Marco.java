ce3238b9e87fa25400625d7a9279a660637fb7b4
mmasias@macbook <manuel@masiasweb.com>
2024-01-02 00:17:45 +0100

    Implementa lógica de viaje en `Marco.java`
    Se agregó la estructura básica para el seguimiento del viaje de Marco
    con variables de distancia inicial, avance de la madre y contabilización
    de días mediante un bucle `while`. Las constantes `INITIAL_DISTANCE` y
    `MOTHER_ADVANCE` establecen las condiciones del viaje y el bucle
    verifica la progresión hasta que se complete la distancia. Este cambio
    prepara el terreno para implementar la lógica específica de avance del
    viaje en futuras actualizaciones.

--- retos&proyectos/viajeMarco/src/vPRG1/Marco.java
+++ retos&proyectos/viajeMarco/src/vPRG1/Marco.java
@@ -1,9 +1,21 @@
 package vPRG1;
+
 import java.util.Scanner;
 
 class Marco {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
+        final double INITIAL_DISTANCE = 350;
+        final double MOTHER_ADVANCE = 80;
+        double remainingDistance = INITIAL_DISTANCE;
+        int days = 0;
+        boolean onJourney = remainingDistance > 0;
+
+        while (onJourney) {
+            days++;
+
+            onJourney = remainingDistance > 0;
+        }
     }
 }
