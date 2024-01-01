9f1a73678e9fd015cba28ebfa15c6957ac07865f
mmasias@macbook <manuel@masiasweb.com>
2024-01-02 00:44:27 +0100

    Incorpora impacto del clima en cálculo de avance
    Mejoramos la simulación de la jornada de Marco al introducir variables que representan la probabilidad de lluvia fuerte y suave, afectando la velocidad de Marco. Cuando la lluvia es detectada, la velocidad de avance se ajusta y se notifica el tipo de lluvia en la historia del día.

--- retos&proyectos/viajeMarco/src/vPRG1/Marco.java
+++ retos&proyectos/viajeMarco/src/vPRG1/Marco.java
@@ -8,6 +8,8 @@ class Marco {
 
         final double INITIAL_DISTANCE = 350;
         final double MOTHER_ADVANCE = 80;
+        final double HARD_RAIN = 0.1;
+        final double SOFT_RAIN = 0.4;
         double remainingDistance = INITIAL_DISTANCE;
         int days = 0;
         boolean onJourney = remainingDistance > 0;
@@ -16,11 +18,25 @@ class Marco {
             days++;
             double marcoVelocity = Math.random() * 5 + 10;
             double marcoTime = Math.random() * 2 + 8;
+
+            double rainProbability = Math.random();
+            double velocityChange = 1;
+            String history = "Buen dia";
+            if (rainProbability <= HARD_RAIN) {
+                velocityChange = 0.25;
+                history = "Lluvia fuerte!";
+            } else if (rainProbability <= SOFT_RAIN) {
+                velocityChange = 0.75;
+                history = "Lluvia suave!";
+            }
+            marcoVelocity = marcoVelocity * velocityChange;
+
             double marcoAdvance = marcoVelocity * marcoTime;
 
             remainingDistance = remainingDistance - marcoAdvance + MOTHER_ADVANCE;
 
             System.out.println("Dia " + days);
+            System.out.println(history);
             System.out.println("Marco avanzó " + marcoAdvance);
             System.out.println("Lo separa de su madre " + remainingDistance + " Km.");
             System.out.println("-".repeat(10));
