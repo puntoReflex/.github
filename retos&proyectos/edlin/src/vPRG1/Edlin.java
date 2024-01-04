ecad1081db8f1594d04e681660cc61ce18fa9061
mmasias@kde.home <manuel@masiasweb.com>
2024-01-05 00:39:19 +0100

    Inicio de desarrollo del editor
    Se implementa la estructura que almacena el documento (la matriz document[]) y una interfaz de usuario básica al editor Edlin consistente en un menú interactivo y la visualización de líneas activas en la consola.
    
    Adicionalmente, se crean los primeros métodos para limpiar la pantalla y dibujar líneas divisorias, lo cual facilita la lectura y el seguimiento de los cambios realizados.
    
    Se aprovecha la matriz document para indicarle al usuario como utilizar el programa.
    
    #idea: Quizá convenga agregar una opción para borrar todo el documento sin tener que ir línea a línea.

--- retos&proyectos/edlin/src/vPRG1/Edlin.java
+++ retos&proyectos/edlin/src/vPRG1/Edlin.java
@@ -4,6 +4,48 @@ import java.util.Scanner;
 
 class Edlin {
     public static void main(String[] args) {
+        int activeLine[] = { 1 };
+        String document[] = {
+                "Bienvenidos al editor EDLIN",
+                "Utilice el menu inferior para editar el texto",
+                "------",
+                "[L] permite definir la linea activa",
+                "[E] permite editar la linea activa",
+                "[I] permite intercambiar dos lineas",
+                "[B] borra el contenido de la linea activa",
+                "[S] sale del programa",
+                "",
+                ""
+        };
 
+        do {
+            print(document, activeLine);
+        } while (processActions(document, activeLine));
     }
+
+    static void print(String[] document, int[] activeLine) {
+        clearScreen();
+        printHorizontalLine();
+        for (int line = 0; line < document.length; line++) {
+            System.out.println(line + separator(line, activeLine[0]) + document[line]);
+        }
+        printHorizontalLine();
+    }
+
+    static String separator(int line, int activeLine) {
+        return line == activeLine ? ":*| " : ": | ";
+    }
+
+    static void printHorizontalLine() {
+        System.out.println("-".repeat(50));
+    }
+
+    static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static boolean processActions(String[] document, int[] activeLine) {
+        return false;
+    }    
 }
