db12827b8e3a6582316d02457f677f2226e4e3ec
mmasias@kde.home <manuel@masiasweb.com>
2024-01-05 01:49:16 +0100

    Implementa la eliminación de una línea
    En este proceso se requiere que el usuario confirme la acción introduciendo el número de la línea activa.

--- retos&proyectos/edlin/src/vPRG1/Edlin.java
+++ retos&proyectos/edlin/src/vPRG1/Edlin.java
@@ -73,6 +73,10 @@ class Edlin {
     }
 
     static void delete(String[] document, int[] activeLine) {
+        System.out.println("Esta acción es irreversible: indique el número de línea activa para confirmarlo ["+activeLine[0]+"]");
+        if (askInt()==activeLine[0]) {
+            document[activeLine[0]]="";
+        }
     }
 
     static void exchangeLines(String[] document) {
@@ -95,12 +99,10 @@ class Edlin {
             activeLine[0] = askInt();
             validLine = activeLine[0] >= 0 && activeLine[0] < document.length;
         } while (!validLine);
-
     }
 
     static int askInt() {
         Scanner input = new Scanner(System.in);
         return input.nextInt();
     }
-
 }
