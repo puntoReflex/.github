c179c5467d105e61565b5a7bd44071d26ba9d153
mmasias@macbook <manuel@masiasweb.com>
2024-01-19 18:59:12 +0100

    Coloca soldados aleatorios en casillas del mar
    Agregamos una nueva constante para definir el número máximo de soldados y un método que distribuye esos soldados aleatoriamente en la matriz que representa el mar.
    
    Removimos temporalmente el mapeo de representación anterior en la impresión para mostrar los valores directamente.
    
    La adición del método `putSoldiers` permite una inicialización aleatoria de la posición de los soldados en el juego.

--- retos&proyectos/rescate/src/vPRG1/Rescue.java
+++ retos&proyectos/rescate/src/vPRG1/Rescue.java
@@ -16,13 +16,17 @@ class Rescue {
                 { -1, -1, -1, -1, -1, -1, -1, -1 }
         };
 
+        final int MAX_SOLDIERS = 16;
+
+        putSoldiers(sea, MAX_SOLDIERS);
+
         printSea(sea);
     }
 
     static void printSea(int[][] sea) {
         for (int row = 0; row < sea.length; row++) {
             for (int col = 0; col < sea[row].length; col++) {
-                System.out.print(map(sea[row][col]));
+                System.out.print(sea[row][col]);
             }
             System.out.println();
         }
@@ -33,4 +37,15 @@ class Rescue {
         return i < 0 ? " ? " : caracter[i];
     }
 
+    static void putSoldiers(int[][] sea, int maxSoldiers) {
+        int soldiersNumber = 0;
+        while (soldiersNumber < maxSoldiers) {
+            int row = (int) (Math.random() * 8);
+            int col = (int) (Math.random() * 8);
+            if (sea[row][col] != -2) {
+                sea[row][col] = -2;
+                soldiersNumber++;
+            }
+        }
+    }
 }
