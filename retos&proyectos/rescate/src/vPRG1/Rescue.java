b32c29c35413a47fcf5d4f04c97d2d1b3dd92dec
mmasias@macbook <manuel@masiasweb.com>
2024-01-19 18:47:58 +0100

    Inicializa estructura básica de simulación
    Se agregó el método principal y la lógica de visualización en la clase Rescue, estableciendo la infraestructura inicial para simular un entorno de rescate en el mar. Se introduce una matriz para representar el mar con valores predeterminados que indican posiciones desconocidas y una función para imprimir el estado actual de la matriz, junto con un mapeo para traducir estados numéricos a caracteres visuales. La convención definida es:
    
    - MAR, DESCONOCIDO: -1
    - SOLDADO, DESCONOCIDO: -2
    - MAR, CONOCIDO: 1
    - SOLDADO, CONOCIDO: 2
    
    Con esta convención, cambiar de conocido a desconocido se reducirá a multiplicar por -1. Y saber si es desconocido se reducirá a ver si es menor que 0.

--- retos&proyectos/rescate/src/vPRG1/Rescue.java
+++ retos&proyectos/rescate/src/vPRG1/Rescue.java
@@ -4,5 +4,33 @@ import java.util.Scanner;
 
 class Rescue {
 
-}
+    public static void main(String[] args) {
+        int[][] sea = {
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1, -1 }
+        };
 
+        printSea(sea);
+    }
+
+    static void printSea(int[][] sea) {
+        for (int row = 0; row < sea.length; row++) {
+            for (int col = 0; col < sea[row].length; col++) {
+                System.out.print(map(sea[row][col]));
+            }
+            System.out.println();
+        }
+    }
+
+    static String map(int i) {
+        String[] caracter = { " ? ", " ~ ", "\\O/" };
+        return i < 0 ? " ? " : caracter[i];
+    }
+
+}
